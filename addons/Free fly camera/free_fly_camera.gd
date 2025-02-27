2b04ec8561310e2201378ebd0c314bf0bc12bbf0
highlife22 <1440980+highlife22@users.noreply.github.com>
2025-02-27 07:47:24 +0100

    Update free_fly_camera.gd

--- addons/Free fly camera/free_fly_camera.gd
+++ addons/Free fly camera/free_fly_camera.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("Free fly camera", "CharacterBody3D", preload("Src/free_fly_startup.gd"), preload("Assets/icon.jpg"))
+	add_custom_type("Free fly camera", "CharacterBody3D", preload("Src/free_fly_startup.gd"), preload("Assets/icon.png"))
 
 func _exit_tree():
 	remove_custom_type("Free fly camera")
